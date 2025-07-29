c0dc17ba620c50a476d47a79b5d6c321c27e6303
Mark Dudding <maduddin@us.ibm.com>
2025-07-29 11:54:26 -0400

    chore update dependencies - removed protractor support.

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '250703',
-    'hash': 'gc4224678'
+    'version': '250729',
+    'hash': '44318839'
   },
   'allow_switching_backend_instance': true
 };
